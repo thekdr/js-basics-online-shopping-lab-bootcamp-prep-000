43960562ecd4589e6010adc5e7b30be21fe192e7
Kristopher Ross <thekdr@yahoo.com>
2018-02-06 17:26:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,5 +54,14 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
+  if (parseInt(cardNumber) > 0) {
+    var total = 0;
+    for (var i = 0; i < cart.length; i++) {
+      total += parseInt((Object.values(cart[i]))[0]);
+    }
+    console.log(`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
+  } else {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
 }
