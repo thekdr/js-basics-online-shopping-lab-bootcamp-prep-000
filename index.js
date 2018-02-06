623f9c152e1c94dbd55af66bbb4ff9a4b7d7d01b
Kristopher Ross <thekdr@yahoo.com>
2018-02-06 15:49:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  cart = { [item]: Math.floor(Math.random() *100);
+  cart = { [item]: Math.floor(Math.random() *100)};
 }
 
 function viewCart() {
