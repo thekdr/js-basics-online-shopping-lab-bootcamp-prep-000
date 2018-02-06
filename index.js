982d7b4f4a6c0353818d14c9f4aac2b9651123a8
Kristopher Ross <thekdr@yahoo.com>
2018-02-06 16:04:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  cart[item] = Math.floor(Math.random() *100);
+  var newItem = { item: Math.floor(Math.random() *100)};
+  cart.push = newItem
   console.log(`${item} has been added to your cart.`);
   return cart;
 }
